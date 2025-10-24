39998501156f9c304c55f3cd07fab46494bb4a90
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-24 22:43:13 +0530

    Refactor Login component to main App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,55 @@
 import "./App.css";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "./firebase.init";
+
+const googleProvider = new GoogleAuthProvider();
 
 function App() {
+  const [user, setUser] = useState(null);
+  const handleGoogleSignIn = () => {
+    // console.log("Google button clicked");
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        console.log(result.user);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("Sign Out Done");
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <>
-      <h1> Simple Auth </h1>
+      <div>
+        <h2>PLease login</h2>
+        {/* <button onClick={handleGoogleSignIn}>Sign in with Google</button>
+      <button onClick={handleSignOut}>Sign Out</button> */}
+
+        {user ? (
+          <button onClick={handleSignOut}>Sign Out</button>
+        ) : (
+          <button onClick={handleGoogleSignIn}>Sign in with Google</button>
+        )}
+
+        {user && (
+          <div>
+            <h3>{user?.displayName}</h3>
+            <h5>Email : {user.email} </h5>
+            <img src={user?.photoURL} alt="" />
+          </div>
+        )}
+      </div>
     </>
   );
 }
